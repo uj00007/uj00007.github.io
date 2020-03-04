e8eeca7bfe6cb9fb7ed133f1c6dd741c8d399b8c
Ujjwal Goyal <ujjwal.goyal@bounceshare.com>
2020-03-04 20:23:14 +0530

    updated latest resume and added ui flex over main screen , fixing ui of main content container

--- src/App.js
+++ src/App.js
@@ -1,48 +1,66 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header/Header';
-import Maintainence from './components/Maintainence/Maintainence';
-import Content from './containers/Content/Content';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Maintainence from "./components/Maintainence/Maintainence";
+import Content from "./containers/Content/Content";
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      selectedTab:1,
-      headingColor:"white"
-    }
+    this.state = {
+      selectedTab: 1,
+      headingColor: "white"
+    };
   }
 
-  tabChange = (tab) => {
-    if(tab === 1 || tab === 5)
-    this.setState({
-      selectedTab: tab,
-      headingColor: "white"
-    });
+  tabChange = tab => {
+    if (tab === 1 || tab === 5)
+      this.setState({
+        selectedTab: tab,
+        headingColor: "white"
+      });
     else
-    this.setState({
-      selectedTab: tab,
-      headingColor: "tomato"
-    });
-  }
+      this.setState({
+        selectedTab: tab,
+        headingColor: "tomato"
+      });
+  };
   render() {
     return (
       <div className="App">
-        <Header tabChange={this.tabChange} headingColor={this.state.headingColor}/>
-        {this.state.selectedTab === 1?
-          <Content />:
-          this.state.selectedTab === 2?
-          <Maintainence />:
-            this.state.selectedTab === 3?
-            <Maintainence />:
-              this.state.selectedTab === 4?
-              <Maintainence />:
-                this.state.selectedTab === 5?
-                <Content />:
-                  this.state.selectedTab === 6 &&
-                  <Maintainence />
-        }
+        <div
+          style={{
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            alignSelf: "center",
+            flexDirection: "column",
+            zIndex: 3,
+            position: "absolute",
+            top: 10,
+            justifyContent: "center",
+            flexWrap: "wrap"
+          }}
+        >
+          <Header
+            tabChange={this.tabChange}
+            headingColor={this.state.headingColor}
+          />
+          {this.state.selectedTab === 1 ? (
+            <Content />
+          ) : this.state.selectedTab === 2 ? (
+            <Maintainence />
+          ) : this.state.selectedTab === 3 ? (
+            <Maintainence />
+          ) : this.state.selectedTab === 4 ? (
+            <Maintainence />
+          ) : this.state.selectedTab === 5 ? (
+            <Content />
+          ) : (
+            this.state.selectedTab === 6 && <Maintainence />
+          )}
+        </div>
       </div>
     );
   }
